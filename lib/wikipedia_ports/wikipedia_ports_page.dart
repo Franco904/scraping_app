68981b2856f7f5053b01b072a44dd80ac3851fbb
Franco904 <francostavares2003@gmail.com>
2024-03-29 21:33:23 -0300

    feat: exibe portas em forma de grid

--- lib/wikipedia_ports/wikipedia_ports_page.dart
+++ lib/wikipedia_ports/wikipedia_ports_page.dart
@@ -16,66 +16,101 @@ class WikipediaPortsPage extends GetView<WikipediaPortsController> {
         backgroundColor: Colors.grey[200],
         shadowColor: Colors.black,
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Align(
-          alignment: Alignment.topCenter,
-          child: ListView(
-            physics: const BouncingScrollPhysics(),
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(height: 40),
-                  const Text('Lista de portas reservadas TCP/IP'),
-                  const SizedBox(height: 8),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: ElevatedButton.icon(
-                      icon: const Icon(Icons.sync, color: Colors.indigo),
-                      label: const Text('Receber dados', style: TextStyle(color: Colors.indigo)),
-                      onPressed: () => controller.getPorts(),
+      body: LayoutBuilder(
+        builder: (_, constraints) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: ListView(
+              physics: const BouncingScrollPhysics(),
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 40),
+                    const Text('Lista de portas reservadas TCP/IP'),
+                    const SizedBox(height: 8),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: ElevatedButton.icon(
+                        icon: const Icon(Icons.sync, color: Colors.indigo),
+                        label: const Text('Receber dados', style: TextStyle(color: Colors.indigo)),
+                        onPressed: () => controller.getPorts(),
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 16),
-                ],
-              ),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16),
-                child: Divider(),
-              ),
-              const SizedBox(height: 16),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 24),
-                child: Obx(() {
+                    const SizedBox(height: 16),
+                  ],
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Divider(),
+                ),
+                const SizedBox(height: 16),
+                Obx(() {
                   return controller.isLoading
                       ? const Center(
                           child: Padding(
                           padding: EdgeInsets.only(top: 48),
-                          child: CircularProgressIndicator(),
+                          child: CircularProgressIndicator(color: Colors.indigo),
                         ))
-                      : Column(
-                          children: controller.ports.map((port) {
-                            return Container(
-                              decoration: BoxDecoration(border: Border.all(color: Colors.black54)),
-                              child: ListTile(
-                                title: Text(port.number),
-                                subtitle: Text(port.description),
-                                trailing: Text(port.status),
-                                tileColor: Colors.grey[200],
+                      : Padding(
+                          padding: const EdgeInsets.only(left: 20, right: 20, bottom: 24),
+                          child: GridView.builder(
+                            physics: const NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                              maxCrossAxisExtent: constraints.maxWidth * 0.25,
+                            ),
+                            itemCount: controller.ports.length,
+                            itemBuilder: (_, i) {
+                              final port = controller.ports[i];
+                              return GestureDetector(
+                                child: Center(
+                                  child: Container(
+                                    width: 64,
+                                    height: 64,
+                                    decoration: BoxDecoration(
+                                      color: Colors.grey[200],
+                                      border: Border.all(color: Colors.black12),
+                                      borderRadius: const BorderRadius.all(Radius.circular(8)),
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        port.number,
+                                        style: Get.textTheme.titleLarge,
+                                      ),
+                                    ),
+                                  ),
+                                ),
                                 onTap: () => Get.dialog(PortDetailsDialog(
                                   portItem: PortItem.fromPort(port),
                                 )),
-                              ),
-                            );
-                          }).toList(),
+                              );
+                            },
+                          ),
                         );
                 }),
-              ),
-            ],
-          ),
-        ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
 }
+
+// Column(
+//                           children: controller.ports.map((port) {
+//                             return Container(
+//                               decoration: BoxDecoration(border: Border.all(color: Colors.black54)),
+//                               child: ListTile(
+//                                 title: Text(port.number),
+//                                 subtitle: Text(port.description),
+//                                 trailing: Text(port.status),
+//                                 tileColor: Colors.grey[200],
+//                                 onTap: () => Get.dialog(PortDetailsDialog(
+//                                   portItem: PortItem.fromPort(port),
+//                                 )),
+//                               ),
+//                             );
+//                           }).toList(),
+//                         );
